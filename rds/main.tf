6f4722d8fda4dbb0a1bdc18a5f292af010c711e1
Evolve Cyber LLC <fsadykov@evolvecyber.com>
2024-01-27 12:14:51 -0600

    removed [

--- rds/main.tf
+++ rds/main.tf
@@ -4,6 +4,5 @@ resource "aws_db_subnet_group" "default" {
     data.terraform_remote_state.this.outputs.private_subnets[0],
     data.terraform_remote_state.this.outputs.private_subnets[1],
     data.terraform_remote_state.this.outputs.private_subnets[2]
-    ]
   ]
 }
